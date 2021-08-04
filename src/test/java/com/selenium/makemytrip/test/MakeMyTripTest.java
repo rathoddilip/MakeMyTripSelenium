d813622c6acff48a1edcfb02006c73f7564da3b5
Arjun <jadhavarjun53@gmail.com>
2021-08-04 17:45:37 +0530

    [DilipRathod] RefactorCode

--- src/test/java/com/selenium/makemytrip/test/MakeMyTripTest.java
+++ src/test/java/com/selenium/makemytrip/test/MakeMyTripTest.java
@@ -23,7 +23,5 @@ public class MakeMyTripTest extends BaseClass {
         dashboard.verify_flight_price();
         dashboard.verify_to_bottom_page_total_price();
         System.out.println(driver.getCurrentUrl());
-        String expectedUrl="https://www.makemytrip.com/flight/search?itinerary=BOM-BLR-03/08/2021_BLR-BOM-10/08/2021&tripType=R&paxType=A-1_C-0_I-0&intl=false&cabinClass=E&ccde=IN&lang=eng";
-        Assert.assertEquals(driver.getCurrentUrl(),expectedUrl);
     }
 }
